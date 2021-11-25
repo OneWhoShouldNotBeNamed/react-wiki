02e4e80d9c37cc09f830891a7e8a0abd0a21c817
Mahesh <maheshsk019@gmail.com>
2021-11-25 21:24:00 +0530

    folder structure done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+import Search from "./components/Search/search";
+import Card from "./components/Cards/cards";
+import Pagination from "./components/Pagination/pagination";
+import Filter from "./components/Filters/filters";
+import Navbar from "./components/Navbar/navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">Card component will be placed here</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
